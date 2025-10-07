7fa38750df3e82f7b7c8f0be3f32894b4a05ded4
Erik Müller <erik@circonomit.de>
2025-10-07 23:19:26 +0200

    chore: add panda for styling

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
 import "solid-devtools";
-
 import App from "./App";
+import "index.css";
 
 const root = document.getElementById("root");
 
